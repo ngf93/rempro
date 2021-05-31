b2b2bfa7804b4465c20170445c10d8b7e0c3fbbf
ngf93 <nabiullinaguzel93@gmail.com>
2021-05-31 18:13:53 +0300

    in process 3

--- js/main.js
+++ js/main.js
@@ -24,12 +24,61 @@ arr_sliders_1.forEach(function(item, i, arr) {
   });
 });
 
-const swiper3 = new Swiper('.swiper-product', {
-  spaceBetween: 10,
-});
+
 
 const swiper4 = new Swiper('.swiper-product-mini', {
+  direction:'horizontal',
+  slidesPerView: 3,
   spaceBetween: 10,
+  height: null,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    576: {
+      spaceBetween: 20,
+      height: 120,
+    },
+    768: {
+      direction:'vertical',
+      height: 200,
+      spaceBetween: 10,
+    },
+    992: {
+      height: 240,
+    },
+    1200: {
+      height: 320,
+    },
+    1400: {
+      height: 380,
+      spaceBetween: 20,
+    }
+  }
+});
+const swiper3 = new Swiper('.swiper-product', {
+  direction:'horizontal',
+  height: 380,
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper4
+  },
+  breakpoints: {
+    768: {
+      direction:'vertical',
+      height: 280,
+    },
+    992: {
+      height: 380,
+    },
+    1200: {
+      height: 440,
+    },
+    1400: {
+      height: 500,
+    }
+  }
 });
 
 
